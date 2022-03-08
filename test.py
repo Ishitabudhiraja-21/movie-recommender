d3be6415bb7e970eb972e0a3bead667713e628ae
Ishitabudhiraja-21 <70087461+Ishitabudhiraja-21@users.noreply.github.com>
2022-03-08 14:53:52 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import requests 
+
+def fetch_poster(movie_id): #takes a movie id and returns detais related to it in this case poster.
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie): #recommend 5 movies based on a given movie
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:6]
+    
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    return recommended_movie_names, recommended_movie_posters
+
+
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+
+st.title('Movie Recommendation System')
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    names,poster=recommend(selected_movie)
+    #layout 
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(poster[0])
+    with col2:
+        st.text(names[1])
+        st.image(poster[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(poster[2])
+    with col4:
+        st.text(names[3])
+        st.image(poster[3])
+    with col5:
+        st.text(names[4])
+        st.image(poster[4])
+
+    
